c25927fafa78994affc2ad341f2a12d86a688618
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-04-22 17:01:17 +0500

    Add frontend and setup react project and tailwind dependencies

---
+++ frontend/space-finder/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Add other extensions as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
